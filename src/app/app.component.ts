d0bb014b2823117647c854ef20a3ae6f44a5debc
Surendhar D(surendhar.d) <surendhar.d@infosys.com>
2023-04-29 11:31:58 +0530

    Initial Setup Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,77 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the forest',
+      url: 'https://images.pexels.com/photos/1179229/pexels-photo-1179229.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the city',
+      url: 'https://images.pexels.com/photos/1563256/pexels-photo-1563256.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the snow',
+      url: 'https://images.pexels.com/photos/688660/pexels-photo-688660.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the forest',
+      url: 'https://images.pexels.com/photos/1179229/pexels-photo-1179229.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the city',
+      url: 'https://images.pexels.com/photos/1563256/pexels-photo-1563256.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the snow',
+      url: 'https://images.pexels.com/photos/688660/pexels-photo-688660.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the forest',
+      url: 'https://images.pexels.com/photos/1179229/pexels-photo-1179229.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the city',
+      url: 'https://images.pexels.com/photos/1563256/pexels-photo-1563256.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the snow',
+      url: 'https://images.pexels.com/photos/688660/pexels-photo-688660.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the forest',
+      url: 'https://images.pexels.com/photos/1179229/pexels-photo-1179229.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the city',
+      url: 'https://images.pexels.com/photos/1563256/pexels-photo-1563256.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+      title: 'At the snow',
+      url: 'https://images.pexels.com/photos/688660/pexels-photo-688660.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+  ];
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
